8871824bab4a07552a9114c5e7bb76c033e23752
Vlad <vlvete1990@yandex.ru>
2022-09-13 21:09:03 +0300

    chore: added contextMenu

--- background.js
+++ background.js
@@ -1,5 +1,24 @@
 chrome.runtime.onInstalled.addListener((details) => {
-    console.log("details", details);
+    chrome.contextMenus.create({
+        title: "test context menu",
+        id: "contextMenu1",
+        contexts: ["page", "selection"]
+    })
+    chrome.contextMenus.onClicked.addListener((event) => {
+        console.log(event);
+    })
+    chrome.contextMenus.create({
+        title: "test context menu 1",
+        id: "contextMenu2",
+        parentId: "contextMenu1",
+        contexts: ["page", "selection"]
+    })
+    chrome.contextMenus.create({
+        title: "test context menu 2",
+        id: "contextMenu3",
+        parentId: "contextMenu1",
+        contexts: ["page", "selection"]
+    })
 });
 
 console.log("background script running");
