c8f315e109c672462814892eb4773c9d904937cf
Vlad <vlvete1990@yandex.ru>
2022-09-13 21:23:48 +0300

    chore: added search query

--- background.js
+++ background.js
@@ -6,18 +6,10 @@ chrome.runtime.onInstalled.addListener((details) => {
     })
     chrome.contextMenus.onClicked.addListener((event) => {
         console.log(event);
-    })
-    chrome.contextMenus.create({
-        title: "test context menu 1",
-        id: "contextMenu2",
-        parentId: "contextMenu1",
-        contexts: ["page", "selection"]
-    })
-    chrome.contextMenus.create({
-        title: "test context menu 2",
-        id: "contextMenu3",
-        parentId: "contextMenu1",
-        contexts: ["page", "selection"]
+        chrome.search.query({
+            disposition: "NEW_TAB",
+            text: event.selectionText,
+        })
     })
 });
 
