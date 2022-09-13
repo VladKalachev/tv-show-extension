c249db1f46b8891a4f9d648f9d2a4963a2027ba8
Vlad <vlvete1990@yandex.ru>
2022-09-13 21:30:04 +0300

    chore: added tabs

--- background.js
+++ background.js
@@ -6,9 +6,17 @@ chrome.runtime.onInstalled.addListener((details) => {
     })
     chrome.contextMenus.onClicked.addListener((event) => {
         console.log(event);
-        chrome.search.query({
-            disposition: "NEW_TAB",
-            text: event.selectionText,
+        // chrome.search.query({
+        //     disposition: "NEW_TAB",
+        //     text: event.selectionText,
+        // })
+        // chrome.tabs.query({
+        //     currentWondow: true,
+        // }, (tabs) => {
+        //     console.log(tabs);
+        // })
+        chrome.tabs.create({
+            url: `https://www.imdb.com/find?q=${event.selectionText}&ref_=nv_sr_sm`
         })
     })
 });
