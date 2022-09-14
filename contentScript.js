1a106634281c1875fe835c18bd969e8546e08e8b
Vlad <vlvete1990@yandex.ru>
2022-09-14 21:23:51 +0300

    chore: added css

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,9 @@
 console.log('Hello Content Script')
+
+const aTags = document.getElementsByTagName('a');
+
+for(const tag of aTags) {
+    if(tag.textContent.includes("i")) {
+        tag.style = "background-color: yellow"
+    }
+}
