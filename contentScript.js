9e080e439f487ca9dd6cda971d9c94f3a6fba120
Vlad <vlvete1990@yandex.ru>
2022-10-09 20:12:03 +0300

    chore: contentScript

--- contentScript.js
+++ contentScript.js
@@ -2,6 +2,7 @@ console.log('Hello Content Script')
 
 const aTags = document.getElementsByTagName('a');
 
+
 for(const tag of aTags) {
     if(tag.textContent.includes("i")) {
         tag.style = "background-color: yellow"
